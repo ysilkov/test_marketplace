0311762aece18dbbf17eac94b4d927becda43701
Olena Lazarenko <alyonalazarenko27@gmail.com>
2023-06-22 13:20:49 +0300

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my branch. You can see the changes now.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hello
         </a>
       </header>
     </div>
